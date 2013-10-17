e76b73f4a0539b2ac65a9e9999df18027f62008c
panosjapan <panos@japancentre.com>
2013-10-17 16:41:52 +0100

    email configuration 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   
   attr_accessible :name, :email, :password, :password_confirmation, :admin, :department, :department_id, :department_name
 
-  validates_presence_of  :email, :name 
+    validates_presence_of  :email, :name 
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email"
   validates_length_of :name, :within => 3..40
   validates_uniqueness_of :email
