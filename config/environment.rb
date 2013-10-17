8da4d81312e1d96ff513f2e814595b5535c66a22
panosjapan <panos@japancentre.com>
2013-10-17 16:00:38 +0100

    email configuration

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 LunchOrder::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'japancentre.com',
+    :authentication => :plain
+}
+ActionMailer::Base.delivery_method = :smtp
